825e2cc23d83bb33683a0fa7a56a6bb554e07ebf
ntestoc3 <ntestoc3@outlook.com>
2020-03-24 23:55:26 +0800

    修改版本号

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ntestoc3/common "2.1.0-SNAPSHOT"
+(defproject ntestoc3/common "2.1.1-SNAPSHOT"
   :description "my common libs helper"
   :url "https://github.com/ntestoc3/common"
   :license {:name "MIT"
