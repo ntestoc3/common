93ee57c672ab7d6127afd5ca827208fce0b97485
ntestoc3 <ntestoc3@outlook.com>
2023-04-22 15:01:22 +0800

    bump dep version

--- project.clj
+++ project.clj
@@ -1,19 +1,20 @@
-(defproject ntestoc3/common "2.1.6"
+(defproject ntestoc3/common "2.1.7"
   :description "my common libs helper"
   :url "https://github.com/ntestoc3/common"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
-                 [clj-http "3.12.1"]
-                 [com.taoensso/timbre "5.1.2"]    ; logging
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [clj-http "3.12.3"]
+                 [com.taoensso/timbre "6.1.0"]    ; logging
                  [me.raynes/fs "1.4.6"]            ;; file util
-                 [net.lingala.zip4j/zip4j "2.7.0"] ;; zip with password
-                 [clojure.java-time "0.3.2"]       ; datetime
-                 [diehard "0.10.3"]                 ;; guard
-                 [com.grammarly/omniconf "0.4.2"]  ;; config
-                 [fipp "0.6.23"] ;; pretty print
+                 [net.lingala.zip4j/zip4j "2.11.5"] ;; zip with password
+                 [clojure.java-time "1.2.0"]       ; datetime
+                 [diehard "0.11.6"]                 ;; guard
+                 [com.grammarly/omniconf "0.4.3"]  ;; config
+                 [fipp "0.6.26"] ;; pretty print
+                 [clojurewerkz/quartzite "2.1.0"] ;; cron
                  ]
   :profiles {:dev {:dependencies
-                   [[midje "1.9.9" :exclusions [org.clojure/clojure]]]
+                   [[midje "1.10.9" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.2.1"]]}}
   :repl-options {:init-ns common.core})
