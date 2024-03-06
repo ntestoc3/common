24e877934b335b3fe242bb66c600ec1a2a73fffc
ntestoc3 <ntestoc3@outlook.com>
2024-03-06 17:07:24 +0800

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ntestoc3/common "2.1.9"
+(defproject ntestoc3/common "2.1.10"
   :description "my common libs helper"
   :url "https://github.com/ntestoc3/common"
   :license {:name "MIT"
@@ -9,12 +9,12 @@
                                     :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3"]
-                 [com.taoensso/timbre "6.3.1"]      ; logging
+                 [com.taoensso/timbre "6.5.0"]      ; logging
                  [me.raynes/fs "1.4.6"]             ;; file util
                  [net.lingala.zip4j/zip4j "2.11.5"] ;; zip with password
                  [clojure.java-time "1.4.2"]        ; datetime
-                 [diehard "0.11.10"]                 ;; guard
-                 [com.grammarly/omniconf "0.4.3"]   ;; config
+                 [diehard "0.11.12"]                 ;; guard
+                 [com.grammarly/omniconf "0.4.4-SNAPSHOT"]   ;; config
                  [fipp "0.6.26"]                    ;; pretty print
                  [clojurewerkz/quartzite "2.2.0"]   ;; cron
 
@@ -23,14 +23,14 @@
 
                  [camel-snake-kebab/camel-snake-kebab "0.4.3"] ;; name convert
 
-                 [metosin/malli "0.13.0"]
+                 [metosin/malli "0.14.0"]
 
                  ;; repl test
                  [com.hyperfiddle/rcf "20220926-202227"]
 
                  [cheshire "5.12.0"]
-                 [org.clojure/spec.alpha "0.3.218"]]
+                 [org.clojure/spec.alpha "0.4.233"]]
   :profiles {:dev {:dependencies
-                   [[midje "1.10.9" :exclusions [org.clojure/clojure]]]
+                   [[midje "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "3.2.1"]]}}
   :repl-options {:init-ns common.core})
